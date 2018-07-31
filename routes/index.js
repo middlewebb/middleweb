9cf3da5097f413be6c1cd58faa09f7b416c3c60b
middlewebb <sweetcandyy18@gmail.com>
2018-07-31 14:17:04 +0300

    MiddleWEB

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res){
+  res.render('index', {
+    title: 'Home'
+  });
+});
+
+router.get('/results', function(req, res){
+  res.render('results', {
+    title: 'Results'
+  });
+});
+
+router.get('/about', function(req, res){
+  res.render('about', {
+    title: 'About'
+  });
+});
+
+router.get('/contact', function(req, res){
+  res.render('contact', {
+    title: 'Contact'
+  });
+});
+
+module.exports = router;
